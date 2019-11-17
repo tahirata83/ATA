a28b828306df4c1c8bd295f92d0477e5210894ed
tahirata <tahiratayev@gmail.com>
2019-11-17 22:52:15 +0300

    araba 2

--- src/day33/Car1.java
+++ src/day33/Car1.java
@@ -8,26 +8,26 @@ public class Car1 {
     public static void main(String[] args) {
         System.out.println(" car");
     }
-        public int speed;
+    public int speed;
 
 
-        public int wheels;
-        public String model;
-        public String color;
-        public double engine;
-        public String state; // new or old
-        public String name;
-        public int count;
+    public int wheels;
+    public String model;
+    public String color;
+    public double engine;
+    public String state; // new or old
+    public String name;
+    public int count;
 
-        public boolean drive() {
-            if (wheels > 0) {
-                return true;
-            } else {
-                return false;
-            }
+    public boolean drive() {
+        if (wheels > 0) {
+            return true;
+        } else {
+            return false;
         }
+    }
 
-        public void repair() {
+    public void repair() {
         if (state.equals("NEW")) {
             System.out.println("Its new, you dont have to repair it");
         } else {
@@ -35,6 +35,4 @@ public class Car1 {
         }
     }
 
-    }
-
-
+}
